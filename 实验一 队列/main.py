0c646c01403da96ef904d0cfbedce78d3ee06953
杨众山 <tobatu762@qq.com>
2021-03-22 21:09:06 +0800

    [+] 实验一 队列

--- 实验一 队列/main.py
+++ 实验一 队列/main.py
@@ -5,29 +5,35 @@ class Queue:
     def __init__(self):
         self.queue = []
     
+    # 插入数据，建立队列
     def buildQue(self):
         data = [int(_) for _ in input("请输出您想插入的数据：").split()]
         for i in data:
             self.queue.append(i)
     
+    # 输出数据
     def output(self):
         res = ""
         for i in self.queue:
             res += str(i) + " "
         print("当前队列为：" + res)
 
+    # 插入数据
     def insert(self):
         x = int(input("请输入您想插入的数据："))
         self.queue.append(x)
 
+    # 删除队首元素
     def dele(self):
         print("删除队首元素：" + str(self.queue[0]))
         del(self.queue[0])
 
+    # 排序
     def sort(self):
         print("队列排序完成")
         self.queue.sort()
 
+    # 修改队列元素
     def change(self):
         i = int(input("请输入您想修改元素的位置："))
         x = int(input("请输入修改后的数据："))
