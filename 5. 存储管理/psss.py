44fb2f896c8316522a547d4c5a4779ea20752d0f
zhong3 <tobatu762@qq.com>
2021-06-07 09:16:47 +0800

    [+] 增加段式管理存储算法

--- 5. 存储管理/psss.py
+++ 5. 存储管理/psss.py
@@ -2,11 +2,162 @@
 # target: 模拟和验证分页/分段/段页存储管理方式的分配与回收
 import random
 
-class Section:
-    pass
+class Segment:
+    def __init__(self):
+        print("*"*15 + "段式管理模拟" + "*"*15)
+        self.size = int(input("请输入内存大小为："))
+        self.start = int(input("请输入起始地址大小为："))
+        self.use = []
+        self.free = []
 
-class SectionStorage:
-    pass
+    def main(self):
+        self.free.append([self.size, self.start, "空闲"])
+        while True:
+            print("*"*15 + "段式存储管理" + "*"*15)
+            print("\t*\t1.内存分配\t*")
+            print("\t*\t2.内存去配\t*")
+            print("\t*\t0.退出    \t*")
+            opt = int(input("\t\t请输入选项[ ]\b\b"))
+            if not opt:
+                break
+            elif opt == 1:
+                print("*"*20 + "分配算法" + "*"*20)
+                print("\t*\t1.最先分配法\t*")
+                print("\t*\t2.最优分配法\t*")
+                print("\t*\t3.最坏分配法\t*")
+                opt1 = int(input("\t\t请输入选项[ ]\b\b"))
+                self.output()
+                if opt1 == 1:
+                    data = [_ for _ in input("请输入作业名及其所需分配的主存大小（单位：KB）：").split()]
+                    part = int(input("请输入分成几段："))
+                    tmp = 0
+                    for i in range(part):
+                        flag = 1
+                        tmp = int(input("剩余{}K的内存，请输入第{}段的大小（单位：KB）：".format(int(data[1]) - tmp, i+1)))
+                        # 直接遍历free，最先分配
+                        for now in self.free:
+                            if int(data[1]) < now[0]:
+                                work = [tmp, now[1], data[0]+"(%s)"%str(i)]
+                                self.use.append(work)
+                                self.use.sort(key=lambda x:x[1])
+                                now[0] -= tmp
+                                now[1] += tmp
+                                print("分配成功！")
+                                flag = 0
+                                self.free.sort(key=lambda x:x[1])
+                                self.output()
+                                break
+                    if flag:
+                        print("发生错误，分配失败！")
+                elif opt1 == 2:
+                    data = [_ for _ in input("请输入作业名及其所需分配的主存大小（单位：KB）：").split()]
+                    part = int(input("请输入分成几段："))
+                    tmp = 0
+                    for i in range(part):
+                        flag = 1
+                        tmp = int(input("剩余{}K的内存，请输入第{}段的大小（单位：KB）：".format(int(data[1]) - tmp, i+1)))
+                        # 先对free进行大小的升序排序，再进行分配，最优分配
+                        self.free.sort(key=lambda x:x[0])
+                        for now in self.free:
+                            if int(data[1]) < now[0]:
+                                work = [tmp, now[1], data[0]+"(%s)"%str(i)]
+                                self.use.append(work)
+                                self.use.sort(key=lambda x:x[1])
+                                now[0] -= tmp
+                                now[1] += tmp
+                                print("分配成功！")
+                                flag = 0
+                                self.free.sort(key=lambda x:x[1])
+                                self.output()
+                                break
+                    if flag:
+                        print("发生错误，分配失败！")
+                elif opt1 == 3:
+                    data = [_ for _ in input("请输入作业名及其所需分配的主存大小（单位：KB）：").split()]
+                    part = int(input("请输入分成几段："))
+                    tmp = 0
+                    for i in range(part):
+                        flag = 1
+                        tmp = int(input("剩余{}K的内存，请输入第{}段的大小（单位：KB）：".format(int(data[1]) - tmp, i+1)))
+                        # 先对free进行大小的降序排序，再进行分配，最坏分配
+                        self.free.sort(key=lambda x:x[0], reverse=True)
+                        for now in self.free:
+                            if int(data[1]) < now[0]:
+                                work = [tmp, now[1], data[0]+"(%s)"%str(i)]
+                                self.use.append(work)
+                                self.use.sort(key=lambda x:x[1])
+                                now[0] -= tmp
+                                now[1] += tmp
+                                print("分配成功！")
+                                flag = 0
+                                self.free.sort(key=lambda x:x[1])
+                                self.output()
+                                break
+                    if flag:
+                        print("发生错误，分配失败！")
+            elif opt == 2:
+                self.output()
+                workName = input("请输入你要回收的作业名：")
+                flag = 1
+                for i in range(len(self.use)):
+                    for now in self.use:
+                        # 寻找作业名
+                        if now[2].startswith(workName):
+                            now[2] = "空闲"
+                            # 判断是否有上界和下界
+                            for nowFree in self.free:
+                                # 上有空闲
+                                if now[0] + now[1] == nowFree[1]:
+                                    nowFree[1] = now[1]
+                                    self.check()
+                                    self.use.remove(now)
+                                    break
+                                # 下有空闲
+                                elif nowFree[0] + nowFree[1] == now[1]:
+                                    nowFree[0] += now[0]
+                                    self.check()
+                                    self.use.remove(now)
+                                    break
+                                # 上下都无空闲
+                                else:
+                                    self.free.append(now)
+                                    self.use.remove(now)
+                                    self.free.sort(key=lambda x:x[1])
+                                    self.check()
+                                    break
+                            flag = 0
+                if not flag:
+                    print("回收成功！")
+                    self.output()
+                else:
+                    print("发生错误，回收失败！")
+
+    # 输出函数
+    def output(self):
+        print("\n" + "*"*20 + "主存分配情况" + "*"*20)
+        print("已分配：\n分区号\t大小(KB)\t起始(KB)\t状态")
+        i = 1
+        for now in self.use:
+            print("{}\t{}\t\t{}\t\t{}".format(i, now[0],now[1],now[2]))
+            i += 1
+        print("未分配：\n分区号\t大小(KB)\t起始(KB)\t状态")
+        for now in self.free:
+            print("{}\t{}\t\t{}\t\t{}".format(i, now[0],now[1],now[2]))
+            i += 1
+
+    # 对free进行check，判断是否存在相连分区
+    def check(self):
+        for i in range(len(self.free) - 1, 0, -1):
+            if self.free[i-1][0] + self.free[i-1][1] == self.free[i][1]:
+                self.free[i-1][0] += self.free[i][0]
+                self.free.remove(self.free[i])
+
+class SegmentStorage:
+    def __init__(self):
+        pass
+
+    def main(self):
+        pass
 
 class Node:
     def __init__(self, name, size):
@@ -115,10 +266,10 @@ def main():
             p = Page()
             p.main()
         elif opt == 2:
-            s = Section()
+            s = Segment()
             s.main()
         elif opt == 3:
-            ss = SectionStorage()
+            ss = SegmentStorage()
             ss.main()
 
 if __name__ == "__main__":
@@ -135,4 +286,26 @@ job2 80
 job2
 2
 job1
+
+2
+256
+40
+1
+1
+jobA 50
+2
+20
+30
+1
+2
+jobB 80
+2
+10
+70
+2
+jobA
+2
+jobB
+0
+0
 '''
